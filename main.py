7fc35bf08203422f99460af0f189aaa01c9388b0
Thone Anlaug <thoneanlaug@gmail.com>
2026-05-06 18:17:53 +0900

    lecture work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import List
 
+# just some text for the class
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
     # Will this be working?
